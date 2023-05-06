a9ca959ef7fe5cd307c8bca1a1f459230ffb2f69
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-05-06 19:57:10 +0300

    sears and state

--- src/views/Cocktails.jsx
+++ src/views/Cocktails.jsx
@@ -1,17 +1,39 @@
-import { SearchForm } from '../components/SearchForm';
-import { Section } from '../components/Section';
-import { CocktailsList } from '../components/CocktailsList';
-import { Loader } from '../components/Loader';
+import { SearchForm } from "../components/SearchForm";
+import { Section } from "../components/Section";
+import { CocktailsList } from "../components/CocktailsList";
+import { Loader } from "../components/Loader";
+import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { searchByName } from "../api/cocktail-service";
 
 export const Cocktails = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [cocktails, setCocktails] = useState(null);
+  const queryParam = searchParams.get("query");
+  console.log(queryParam);
+  const onFormSubmit = (query) => {
+    setSearchParams({ query: query.trim() });
+  };
+  useEffect(() => {
+    if (queryParam) {
+      searchByName(queryParam)
+        .then((response) => {
+          //console.log(response);
+          setCocktails(response.drinks);
+        })
+        .catch((error) => console.log(error.message));
+    }
+  }, [queryParam]);
+
   return (
     <>
       <Section>
-        <h1 className='uppercase text-4xl text-gray-600 text-center'>
+        <h1 className="uppercase text-4xl text-gray-600 text-center">
           Search Cocktails
         </h1>
 
-        <SearchForm />
+        <SearchForm onFormSubmit={onFormSubmit} />
+        <CocktailsList cocktails={cocktails} />
       </Section>
     </>
   );
