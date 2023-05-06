01b6999bbacd16d5712470f6785b59a258de2b44
Yaroslav Rebrina <yaroslavrebrina@gmail.com>
2023-05-06 19:23:24 +0300

    part 1 done

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,16 +1,26 @@
-import { CocktailsList } from '../components/CocktailsList';
-import { Section } from '../components/Section';
-import { Loader } from '../components/Loader';
+import { CocktailsList } from "../components/CocktailsList";
+import { Section } from "../components/Section";
+import { Loader } from "../components/Loader";
+import { getTrendingCocktails } from "../api/cocktail-service";
+import { useEffect, useState } from "react";
 
 export const Home = () => {
+  const [drinks, setDrinks] = useState(null);
+
+  useEffect(() => {
+    getTrendingCocktails().then((response) =>
+      setDrinks(response.map((item) => item.drinks[0]))
+    );
+  }, []);
+
   return (
     <>
       <Section>
-        <h1 className='text-center font-black text-gray-700 text-4xl mb-10'>
+        <h1 className="text-center font-black text-gray-700 text-4xl mb-10">
           Trending cocktails
         </h1>
 
-        {/* <CocktailsList cocktails={cocktails} /> */}
+        <CocktailsList cocktails={drinks} />
       </Section>
     </>
   );
