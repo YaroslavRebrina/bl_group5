024eedd91928aed82e8c2d0e17c1d438e34ca683
Serhii Kulbashnyi <twitch.live.ua@gmail.com>
2023-05-06 20:35:39 +0300

    Add CocktailCard

--- src/views/CocktailDetail.jsx
+++ src/views/CocktailDetail.jsx
@@ -1,14 +1,28 @@
-import { Section } from '../components/Section';
-import { Loader } from '../components/Loader';
-import { GoBackBtn } from '../components/GoBackBtn';
-import { CocktailInfo } from '../components/CocktailInfo';
-import { useLocation } from 'react-router-dom';
-import { routes } from '../routes';
+import { Section } from "../components/Section";
+import { Loader } from "../components/Loader";
+import { GoBackBtn } from "../components/GoBackBtn";
+import { CocktailInfo } from "../components/CocktailInfo";
+import { useLocation, useParams } from "react-router-dom";
+import { routes } from "../routes";
+import { useEffect, useState } from "react";
+import { getCocktailDetail } from "../api/cocktail-service";
 
 export const CocktailDetail = () => {
+  // const location = useLocation();
+
+  const [cocktailInfo, setCocktailInfo] = useState(null);
+  const { cocktailId } = useParams();
+
+  useEffect(() => {
+    getCocktailDetail(cocktailId).then(setCocktailInfo).catch(console.log);
+  }, [cocktailId]);
+
   return (
-    <h1 className='uppercase text-4xl text-gray-600 text-center'>
-      CocktailDetail
-    </h1>
+    <>
+      <h1 className="uppercase text-4xl text-gray-600 text-center">
+        CocktailDetail
+      </h1>
+      {cocktailInfo && <CocktailInfo {...cocktailInfo} />}
+    </>
   );
 };
